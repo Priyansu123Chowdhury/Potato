2b7d7df2da059dbd98cb04745ce92ce279153ce4
Priyansu123Chowdhury <102274788+Priyansu123Chowdhury@users.noreply.github.com>
2024-08-28 02:45:14 +0530

    Home-Page FrontEnd

--- Potato/src/App.jsx
+++ Potato/src/App.jsx
@@ -1,10 +1,20 @@
-import Navbar from "./cpmponents/Navbar/Navbar"
-
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Footer from './components/Footer/Footer'
+import Navbar from "./components/Navbar/Navbar"
+import Cart from './pages/Cart/Cart'
+import Home from './pages/Home/Home'
+import PlaceOrder from './pages/PlaceOrder/PlaceOrder'
 const App = () => {
-  return (
+  return (<>
     <div className="app">
       <Navbar></Navbar>
+      <Routes>
+        <Route path='/' element={<Home />}></Route>
+        <Route path='/cart' element={<Cart />} ></Route>
+        <Route path='/order' element={<PlaceOrder />} /> </Routes>
     </div>
+    <Footer></Footer></>
   )
 }
 
--- Potato/src/components/Navbar/Navbar.jsx
+++ Potato/src/components/Navbar/Navbar.jsx
@@ -16,6 +16,7 @@ const Navbar = () => {
         <img src={assets.search_icon} alt="" />
         <div className="navbar-search-icon">
           <img src={assets.basket_icon} alt="" />
+          <div className="dot"></div>
         </div>
         <button>Sign In</button>
       </div>
